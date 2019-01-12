c3e5644c02ed9e7602d02a0642a267ad686f1c7e
thufir <hawat.thufir@gmail.com>
2019-01-12 01:47:20 -0800

    incremental

--- src/main/java/xalan/App.java
+++ src/main/java/xalan/App.java
@@ -1,14 +1,26 @@
 package xalan;
 
+import java.net.URI;
+import java.util.Properties;
+import java.util.logging.Logger;
+
 public class App {
 
+    private static final Logger LOG = Logger.getLogger(App.class.getName());
+
     private X x = new X();
-    
+    private Properties properties = new Properties();
+
     public static void main(String[] args) throws Exception {
         new App().transform();
     }
 
-    public void transform() throws Exception {
-        x.transform();
+    private void transform() throws Exception {
+        properties.loadFromXML(App.class.getResourceAsStream("/jaxb.xml"));
+        URI xmlFileURI = new URI(properties.getProperty("xml"));
+        URI xsdFileURI = new URI(properties.getProperty("xsd"));
+        URI xslFileURI = new URI(properties.getProperty("xsl"));
+        URI outputFileURI = new URI(properties.getProperty("output"));
     }
+
 }
--- src/main/java/xalan/X.java
+++ src/main/java/xalan/X.java
@@ -1,6 +1,8 @@
 package xalan;
 
+import java.io.File;
 import java.io.FileOutputStream;
+import java.net.URI;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
@@ -11,7 +13,13 @@ public class X {
     public X() {
     }
 
-    public void transform() throws Exception {
+    public void transform(URI xml, URI xsl, URI out) throws Exception {
+        TransformerFactory tFactory = TransformerFactory.newInstance();
+        Transformer transformer = tFactory.newTransformer(new StreamSource(new File(xsl)));
+        transformer.transform(new StreamSource(new File(xml)), new StreamResult(new FileOutputStream(new File(out))));
+    }
+
+    public void transform2(URI xml, URI xsl, URI out) throws Exception {
         TransformerFactory tFactory = TransformerFactory.newInstance();
         Transformer transformer = tFactory.newTransformer(new StreamSource("/home/thufir/jaxb/xalan/foo.xsl"));
         transformer.transform(new StreamSource("/home/thufir/jaxb/xalan/foo.xml"), new StreamResult(new FileOutputStream("/home/thufir/jaxb/xalan/foo.out")));
